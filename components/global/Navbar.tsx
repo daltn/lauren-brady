57c7d74aa511175ddab761c7a4dae8ed3a37d41e
Dalton Saffe <daltonsaffe@gmail.com>
2023-02-08 20:43:39 -0500

    mod mobile title size

--- components/global/Navbar.tsx
+++ components/global/Navbar.tsx
@@ -31,7 +31,7 @@ export function Navbar({ menuItems }: NavbarProps) {
                 {menuItem.title}
               </Link>
               {idx === 0 && (
-                <div className="absolute top-[55px] text-gray-700 sm:text-sm md:text-lg">
+                <div className="absolute top-[50px] text-sm text-gray-700 md:top-[55px] md:text-lg">
                   Director
                 </div>
               )}
